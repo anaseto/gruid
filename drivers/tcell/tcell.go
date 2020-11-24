0a26d4942b38b4680b24ef252a3be22f445eb120
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2020-11-24 17:57:52 +0000

    add tk and tcell drivers

---
+++ drivers/tcell/tcell.go
@@ -0,0 +1,112 @@
+package tcell
+
+import (
+	"github.com/anaseto/gorltk"
+	"github.com/gdamore/tcell/v2"
+)
+
+type StyleManager interface {
+	// GetAttributes returns a mask of text attributes for a given cell
+	// style.
+	GetStyle(gorltk.GridCell) tcell.Style
+}
+
+type Driver struct {
+	// AttributeGetter has to be provided to make use of tcell text
+	// attributes.
+	StyleManager StyleManager
+	screen       tcell.Screen
+}
+
+func (t *Driver) Init() error {
+	screen, err := tcell.NewScreen()
+	t.screen = screen
+	if err != nil {
+		return err
+	}
+	err = t.screen.Init()
+	if err != nil {
+		return err
+	}
+	t.screen.SetStyle(tcell.StyleDefault)
+	t.screen.EnableMouse()
+	return nil
+}
+
+func (t *Driver) Close() {
+	t.screen.Fini()
+}
+
+func (t *Driver) Flush(gd gorltk.GridDrawer) {
+	for _, cdraw := range gd.FrameCells() {
+		c := cdraw.Cell
+		st := t.StyleManager.GetStyle(c)
+		t.screen.SetContent(cdraw.Pos.X, cdraw.Pos.Y, c.R, nil, st)
+	}
+	//ui.g.Printf("%d %d %d", ui.g.DrawFrame, ui.g.DrawFrameStart, len(ui.g.DrawLog))
+	t.screen.Show()
+}
+
+func (t *Driver) Interrupt() {
+	t.screen.PostEvent(tcell.NewEventInterrupt(nil))
+}
+
+func (t *Driver) PollEvent() (ev gorltk.Event) {
+	for {
+		switch tev := t.screen.PollEvent().(type) {
+		case *tcell.EventKey:
+			switch tev.Key() {
+			case tcell.KeyEsc:
+				ev.Key = " "
+			case tcell.KeyLeft:
+				// TODO: will not work if user changes keybindings
+				ev.Key = "4"
+			case tcell.KeyDown:
+				ev.Key = "2"
+			case tcell.KeyUp:
+				ev.Key = "8"
+			case tcell.KeyRight:
+				ev.Key = "6"
+			case tcell.KeyPgUp:
+				ev.Key = "u"
+			case tcell.KeyPgDn:
+				ev.Key = "d"
+			case tcell.KeyDelete:
+				ev.Key = "5"
+			case tcell.KeyCtrlW:
+				ev.Key = "W"
+			case tcell.KeyCtrlQ:
+				ev.Key = "Q"
+			case tcell.KeyCtrlP:
+				ev.Key = "m"
+			case tcell.KeyEnter:
+				ev.Key = "."
+			}
+			if tev.Rune() != 0 && ev.Key == "" {
+				ev.Key = string(tev.Rune())
+			}
+			ev.Type = gorltk.EventKeyDown
+			return ev
+		case *tcell.EventMouse:
+			x, y := tev.Position()
+			ev.MousePos = gorltk.Position{X: x, Y: y}
+			switch tev.Buttons() {
+			case tcell.Button1:
+				ev.Button = gorltk.ButtonMain
+			case tcell.Button2:
+				ev.Button = gorltk.ButtonAuxiliary
+			case tcell.Button3:
+				ev.Button = gorltk.ButtonSecondary
+			}
+			ev.Type = gorltk.EventMouseDown
+			return ev
+		case *tcell.EventInterrupt:
+			ev.Type = gorltk.EventInterrupt
+			return ev
+		}
+	}
+}
+
+func (t *Driver) ClearCache() {
+	// no special cache
+}
---
+++ drivers/tk/tk.go
@@ -0,0 +1,215 @@
+package tk
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"image"
+	"image/draw"
+	"image/png"
+	"unicode/utf8"
+
+	"github.com/anaseto/gorltk"
+	"github.com/nsf/gothic"
+)
+
+type TileManager interface {
+	// GetImage returns the image to be used for a given cell style.
+	GetImage(gorltk.GridCell) *image.RGBA
+}
+
+type Driver struct {
+	TileManager TileManager
+	ir          *gothic.Interpreter
+	cache       map[gorltk.GridCell]*image.RGBA
+	Width       int
+	Height      int
+	width       int
+	height      int
+	mousepos    gorltk.Position
+	canvas      *image.RGBA
+}
+
+func (tk *Driver) Init() error {
+	tk.canvas = image.NewRGBA(image.Rect(0, 0, tk.Width*16, tk.Height*24))
+	tk.ir = gothic.NewInterpreter(fmt.Sprintf(`
+wm title . "Harmonist Tk"
+wm resizable . 0 0
+set width [expr {16 * %d}]
+set height [expr {24 * %d}]
+wm geometry . =${width}x$height
+set can [canvas .c -width $width -height $height -background #002b36]
+grid $can -row 0 -column 0
+focus $can
+image create photo gamescreen -width $width -height $height -palette 256/256/256
+image create photo bufscreen -width $width -height $height -palette 256/256/256
+$can create image 0 0 -anchor nw -image gamescreen
+`, tk.Width, tk.Height))
+	tk.initElements()
+	tk.ir.RegisterCommand("GetKey", func(c, keysym string) {
+		var s string
+		if c != "" {
+			s = c
+		} else {
+			s = keysym
+		}
+		if len(eventCh) < cap(eventCh) {
+			eventCh <- gorltk.Event{Type: gorltk.EventKeyDown, Key: s}
+		}
+	})
+	tk.ir.RegisterCommand("MouseDown", func(x, y, n int) {
+		if len(eventCh) < cap(eventCh) {
+			eventCh <- gorltk.Event{Type: gorltk.EventMouseDown, MousePos: gorltk.Position{X: (x - 1) / tk.width, Y: (y - 1) / tk.height}, Button: gorltk.MouseButton(n - 1)}
+		}
+	})
+	tk.ir.RegisterCommand("MouseMotion", func(x, y int) {
+		nx := (x - 1) / tk.width
+		ny := (y - 1) / tk.height
+		if nx != tk.mousepos.X || ny != tk.mousepos.Y {
+			if len(eventCh) < cap(eventCh) {
+				tk.mousepos.X = nx
+				tk.mousepos.Y = ny
+				eventCh <- gorltk.Event{Type: gorltk.EventMouseMove, MousePos: gorltk.Position{X: nx, Y: ny}}
+			}
+		}
+	})
+	tk.ir.RegisterCommand("OnClosing", func() {
+		if len(eventCh) < cap(eventCh) {
+			// TODO: make it not depend on a default string and normal mode
+			eventCh <- gorltk.Event{Type: gorltk.EventKeyDown, Key: "S"}
+		}
+	})
+	tk.ir.Eval(`
+bind .c <Key> {
+	GetKey %A %K
+}
+bind .c <Motion> {
+	MouseMotion %x %y
+}
+bind .c <ButtonPress> {
+	MouseDown %x %y %b
+}
+wm protocol . WM_DELETE_WINDOW OnClosing
+`)
+
+	//SolarizedPalette()
+	//settingsActions = append(settingsActions, toggleTiles)
+	//GameConfig.Tiles = true
+	return nil
+}
+
+func (tk *Driver) initElements() error {
+	tk.width = 16
+	tk.height = 24
+	tk.cache = make(map[gorltk.GridCell]*image.RGBA)
+	return nil
+}
+
+var eventCh chan gorltk.Event
+
+func init() {
+	eventCh = make(chan gorltk.Event, 5)
+}
+
+func (tk *Driver) Interrupt() {
+	eventCh <- gorltk.Event{Type: gorltk.EventInterrupt}
+}
+
+func (tk *Driver) Close() {
+}
+
+type rectangle struct {
+	xmin, xmax, ymin, ymax int
+}
+
+func (tk *Driver) Flush(gd gorltk.GridDrawer) {
+	w, h := gd.Size()
+	rects := []rectangle{}
+	r := rectangle{w - 1, 0, h - 1, 0}
+	n := 0
+	for _, cdraw := range gd.FrameCells() {
+		cs := cdraw.Cell
+		x, y := cdraw.Pos.X, cdraw.Pos.Y
+		tk.draw(gd, cs, x, y)
+		if n > 0 && y-r.ymax > 2 {
+			rects = append(rects, r)
+			n = 0
+			r = rectangle{w - 1, 0, h - 1, y}
+		}
+		if x < r.xmin {
+			r.xmin = x
+		}
+		if x > r.xmax {
+			r.xmax = x
+		}
+		if y < r.ymin {
+			r.ymin = y
+		}
+		if y > r.ymax {
+			r.ymax = y
+		}
+		n++
+	}
+	rects = append(rects, r)
+	for _, r := range rects {
+		tk.UpdateRectangle(r.xmin, r.ymin, r.xmax, r.ymax)
+	}
+}
+
+func (tk *Driver) UpdateRectangle(xmin, ymin, xmax, ymax int) {
+	if xmin > xmax || ymin > ymax {
+		return
+	}
+	pngbuf := &bytes.Buffer{}
+	subimg := tk.canvas.SubImage(image.Rect(xmin*16, ymin*24, (xmax+1)*16, (ymax+1)*24))
+	png.Encode(pngbuf, subimg)
+	png := base64.StdEncoding.EncodeToString(pngbuf.Bytes())
+	tk.ir.Eval("gamescreen put %{0%s} -format png -to %{1%d} %{2%d} %{3%d} %{4%d}", png,
+		xmin*16, ymin*24, (xmax+1)*16, (ymax+1)*24) // TODO: optimize this more
+}
+
+func (tk *Driver) draw(gd gorltk.GridDrawer, cs gorltk.GridCell, x, y int) {
+	var img *image.RGBA
+	if im, ok := tk.cache[cs]; ok {
+		img = im
+	} else {
+		img = tk.TileManager.GetImage(cs)
+		tk.cache[cs] = img
+	}
+	draw.Draw(tk.canvas, image.Rect(x*tk.width, tk.height*y, (x+1)*tk.width, (y+1)*tk.height), img, image.Point{0, 0}, draw.Over)
+}
+
+func (tk *Driver) ClearCache() {
+	for c, _ := range tk.cache {
+		//if c.Style == StyleMap {
+		delete(tk.cache, c)
+		//}
+	}
+}
+
+func (tk *Driver) PollEvent() (ev gorltk.Event) {
+	ev = <-eventCh
+	switch ev.Key {
+	case "KP_Enter", "Return", "\r", "\n":
+		ev.Key = "."
+	case "Left", "KP_Left":
+		ev.Key = "4"
+	case "Right", "KP_Right":
+		ev.Key = "6"
+	case "Up", "KP_Up", "BackSpace":
+		ev.Key = "8"
+	case "Down", "KP_Down":
+		ev.Key = "2"
+	case "KP_Prior", "Prior":
+		ev.Key = "u"
+	case "KP_Next", "Next":
+		ev.Key = "d"
+	case "KP_Begin", "KP_Delete":
+		ev.Key = "5"
+	default:
+		if utf8.RuneCountInString(ev.Key) != 1 {
+			ev.Key = ""
+		}
+	}
+	return ev
+}
