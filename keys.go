2c2fff54f30f1740e7ae125e5ba18a07959a93d0
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2020-11-26 16:04:42 +0000

    add key mappings file

---
+++ keys.go
@@ -0,0 +1,21 @@
+package gorltk
+
+type Key string
+
+const (
+	KeyArrowDown  Key = "ArrowDown"
+	KeyArrowLeft  Key = "ArrowLeft"
+	KeyArrowRight Key = "ArrowRight"
+	KeyArrowUp    Key = "ArrowUp"
+	KeyBackspace  Key = "Backspace"
+	KeyDelete     Key = "Delete"
+	KeyEnd        Key = "End"
+	KeyEnter      Key = "Enter"
+	KeyEscape     Key = "Escape"
+	KeyHome       Key = "Home"
+	KeyInsert     Key = "Insert"
+	KeyPageDown   Key = "PageDown"
+	KeyPageUp     Key = "PageUp"
+	KeySpace      Key = " "
+	KeyTab        Key = "Tab"
+)
